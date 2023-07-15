468c47e67bcc329726b7d652f2452f10cad88b0f
Michael Rowsey <mike@rowsey.org>
2023-07-15 10:05:07 -0700

    final

--- js/speller.js
+++ js/speller.js
@@ -4,6 +4,7 @@ export default {
 };
 
 var elements;
+var symbols = {};
 
 await loadPeriodicTable();
 
@@ -12,19 +13,74 @@ await loadPeriodicTable();
 
 async function loadPeriodicTable() {
 	elements = await (await fetch("periodic-table.json")).json();
+	for (let element of elements) {
+		symbols[element.symbol.toLowerCase()] = element;
+	}
 }
 
-function check(inputWord) {
-	// TODO: determine if `inputWord` can be spelled
-	// with periodic table symbols; return array with
-	// them if so (empty array otherwise)
+function findCandidates(inputWord) {
+	var oneLetterSymbols = [];
+	var twoLetterSymbols = [];
 
+	for (let i = 0; i < inputWord.length; i++) {
+		// collect one-letter candidates
+		if (inputWord[i] in symbols && !oneLetterSymbols.includes(inputWord[i])) {
+		oneLetterSymbols.push(inputWord[i]);	
+		}
+		// collect two-letter candidates
+		if (i <= (inputWord.length - 2)) {
+			let two = inputWord.slice(i, i + 2);
+			if (two in symbols && !twoLetterSymbols.includes(two)) {
+				twoLetterSymbols.push(two);
+			}
+		}
+	}
+	return [ ...twoLetterSymbols, ...oneLetterSymbols ];
+}
+
+function spellWord(candidates, charsLeft) {
+	if (charsLeft.length == 0) {
+		return [];
+	} else {
+		if (charsLeft.length >= 2) {
+			let two = charsLeft.slice(0, 2);
+			let rest = charsLeft.slice(2);
+			// found a match?
+			if (candidates.includes(two)) {
+				if (rest.length > 0) {
+					let result = [ two, ...spellWord(candidates, rest) ];
+					if (result.join("") == charsLeft) {
+						return result; 
+					}
+				} else {
+					return [ two ];
+				}
+			}
+		}
+		// now check for one letter symbols
+		if (charsLeft.length >= 1) {
+			let one = charsLeft[0];
+			let rest = charsLeft.slice(1);
+			if (candidates.includes(one)) {
+				if (rest.length > 0) {
+					let result = [ one, ...spellWord(candidates, rest) ];
+					if (result.join("") == charsLeft) {
+						return result; 
+					}
+				} else {
+					return [ one ];
+				}
+			}
+		}
+	}
 	return [];
 }
 
-function lookup(elementSymbol) {
-	// TODO: return the element entry based on specified
-	// symbol (case-insensitive)
+function check(inputWord) {
+	var candidates = findCandidates(inputWord);
+	return spellWord(candidates, inputWord);
+}
 
-	return {};
+function lookup(elementSymbol) {
+	return symbols[elementSymbol];
 }
